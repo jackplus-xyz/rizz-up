018bd60b2e981d0cb5a60192cdeb0fd8b61e8418
Jack Huang <jackplus.xyz@icloud.com>
2024-06-04 10:39:46 -0700

    feat: add ArrowLink

---
+++ client/src/lib/components/ArrowLink.svelte
@@ -0,0 +1,13 @@
+<script>
+	import GuidanceLeft2ShortArrow from '~icons/guidance/left-2-short-arrow';
+
+	export let link = '/';
+	export let text = 'Home';
+</script>
+
+<a href={link} class="group relative flex items-center">
+	{text}
+	<span class="ml-2 transition-all group-hover:translate-x-2 md:ml-4">
+		<GuidanceLeft2ShortArrow />
+	</span>
+</a>
