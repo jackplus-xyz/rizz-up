c69181c269006f5c619bee50e9390392bc3d3e1b
Jack Huang <jackplus.xyz@icloud.com>
2024-06-04 16:09:28 -0700

    style: imporve dark theme ui support

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <div class="min-h-screen text-primary">
   <Header />
   <main
-    class="h-full w-full bg-gradient-to-br from-neutral-50 to-neutral-100 dark:from-neutral-700 dark:to-neutral-800"
+    class="h-full w-full bg-gradient-to-br from-neutral-50 to-neutral-100 dark:from-neutral-800 dark:to-neutral-900"
   >
     <slot />
   </main>
