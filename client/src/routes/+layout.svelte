0e12cb9aaba6bec00ac72ad87df0d75abad6e629
Jack Huang <jackplus.xyz@icloud.com>
2024-06-26 11:46:56 -0700

    feat: add debug-screens

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -2,12 +2,14 @@
   import "../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  const ENV = import.meta.env.VITE_ENV;
 </script>
 
 <div class="min-h-screen text-primary">
   <Header />
   <main
     class="h-full w-full bg-gradient-to-br from-neutral-50 to-neutral-100 dark:from-neutral-800 dark:to-neutral-900"
+    class:debug-screens={ENV === "development"}
   >
     <slot />
   </main>
