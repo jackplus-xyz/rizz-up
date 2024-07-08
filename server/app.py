b064d8a813c7bd1595e52a2b1fb44154a1923c09
jackplus-xyz <jackplus-xyz@icloud.com>
2024-07-08 09:39:55 -0700

    fix: use a list for cors origins

--- server/app.py
+++ server/app.py
@@ -1,14 +1,12 @@
 import os
-
 from flask import Flask
 from flask_cors import CORS
 from image import bp as image_bp
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-ENV = os.getenv("ENV")
+ENV = os.getenv("ENV", "production")
 SERVER_PORT = os.getenv("SERVER_PORT")
 FRONTEND_PORT = os.getenv("FRONTEND_PORT")
 PROD_FRONTEND_URL = os.getenv("PROD_FRONTEND_URL")
@@ -21,10 +19,11 @@ frontend_url = (
 app = Flask(__name__)
 app.register_blueprint(image_bp)
 
-CORS(app, origins=frontend_url)
+# Use a list for origins
+CORS(app, origins=[frontend_url])
 
 if __name__ == "__main__":
     if is_development and SERVER_PORT:
-        app.run(debug=is_development, port=int(SERVER_PORT))
+        app.run(debug=True, port=int(SERVER_PORT))
     else:
-        app.run(debug=is_development)
+        app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
