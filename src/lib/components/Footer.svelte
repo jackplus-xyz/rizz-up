39f254efb61c3e2a2281e8d116fe0a7ca1e8d2b3
jackplus-xyz <jackplus-xyz@icloud.com>
2024-07-10 11:51:14 -0700

    refactor: remove unused links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
-	import Facebook from '~icons/mdi/facebook';
-	import Instagram from '~icons/mdi/instagram';
-	import Twitter from '~icons/mdi/twitter';
-	import Github from '~icons/mdi/github';
+  import Github from "~icons/mdi/github";
 </script>
 
 <footer
-	class="flex h-36 w-full flex-col items-center justify-center bg-neutral-900 text-neutral-200 md:h-48 md:space-y-4"
+  class="flex h-36 w-full flex-col items-center justify-center bg-neutral-900 text-neutral-200 md:h-48 md:space-y-4"
 >
-	<div
-		class="flex flex-col items-center justify-center space-y-2 text-sm md:flex-row md:space-x-4 md:space-y-0 md:text-base"
-	>
-		<div class="flex items-center justify-center space-x-4">
-			<a href="/" class="hover:text-white">
-				<span class="mr-2 font-serif text-xl font-bold italic md:text-2xl">Rizz Up</span>
-			</a>
-			<a href="https://www.facebook.com/rizzup" class="hover:text-white"><Facebook /></a>
-			<a href="https://www.twitter.com/rizzup" class="hover:text-white"><Twitter /></a>
-			<a href="https://www.instagram.com/rizzup" class="hover:text-white"><Instagram /></a>
-			<a href="https://github.com/jackplus-xyz/rizz-up" class="hover:text-white"><Github /></a>
-		</div>
-		<div class="flex space-x-4 py-2">
-			<a href="/terms" class="hover:text-white">Terms of Use</a>
-			<a href="/privacy" class="hover:text-white">Privacy Policy</a>
-		</div>
-	</div>
-	<p class="text-xs text-neutral-500 md:text-sm">&copy; 2024 Rizz Up. All rights reserved.</p>
+  <div
+    class="flex flex-col items-center justify-center space-y-2 text-sm md:flex-row md:space-x-4 md:space-y-0 md:text-base"
+  >
+    <div class="flex items-center justify-center space-x-4">
+      <a href="/" class="hover:text-white">
+        <span class="mr-2 font-serif text-xl font-bold italic md:text-2xl"
+          >Rizz Up</span
+        >
+      </a>
+      <a href="https://github.com/jackplus-xyz/rizz-up" class="hover:text-white"
+        ><Github /></a
+      >
+    </div>
+    <div class="flex space-x-4 py-2">
+      <a href="/terms" class="hover:text-white">Terms of Use</a>
+      <a href="/privacy" class="hover:text-white">Privacy Policy</a>
+    </div>
+  </div>
+  <p class="text-xs text-neutral-500 md:text-sm">
+    &copy; 2024 Rizz Up. All rights reserved.
+  </p>
 </footer>
