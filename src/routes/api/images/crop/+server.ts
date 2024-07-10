85a0cb1ece6d99eeb00937534a94f8b538104dde
jackplus-xyz <jackplus-xyz@icloud.com>
2024-07-09 21:02:55 -0700

    refactor: remove google-auth

--- src/routes/api/images/crop/+server.ts
+++ src/routes/api/images/crop/+server.ts
@@ -1,46 +1,17 @@
-import { dev } from "$app/environment";
-import {
-  GOOGLE_CLOUD_RUN_URL,
-  GOOGLE_CLOUD_PROJECT_ID,
-  GOOGLE_CLOUD_CLIENT_EMAIL,
-  GOOGLE_CLOUD_PRIVATE_KEY,
-} from "$env/static/private";
+import { GOOGLE_CLOUD_RUN_URL } from "$env/static/private";
 import type { RequestHandler } from "@sveltejs/kit";
-import { GoogleAuth } from "google-auth-library";
-
-async function getAuthenticatedClient() {
-  const auth = new GoogleAuth({
-    credentials: {
-      private_key: GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: GOOGLE_CLOUD_CLIENT_EMAIL,
-      project_id: GOOGLE_CLOUD_PROJECT_ID,
-    },
-    scopes: "https://www.googleapis.com/auth/cloud-platform",
-  });
-  return auth.getClient();
-}
 
 export const POST: RequestHandler = async ({ request }) => {
   try {
     const formData = await request.formData();
-    const client = await getAuthenticatedClient();
-    const authHeaders = await client.getRequestHeaders();
-    const apiUrl = dev
-      ? "http://localhost:8080/api/images/crop"
-      : `${GOOGLE_CLOUD_RUN_URL}/api/images/crop`;
 
-    const headers = new Headers(authHeaders);
-    // Remove the Content-Type header if it exists
-    // This allows the browser to set the correct Content-Type with boundary for FormData
-    headers.delete("Content-Type");
-
-    const response = await fetch(apiUrl, {
+    const response = await fetch(`${GOOGLE_CLOUD_RUN_URL}/api/images/crop`, {
       method: "POST",
-      headers: headers,
       body: formData,
     });
 
     if (!response.ok) {
+      console.error(response);
       return new Response("Failed to process image", {
         status: response.status,
       });
